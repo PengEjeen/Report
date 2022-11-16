07abfd447e9eb6e93bd412272f55586cc60083fc
cny <chony5093@gmail.com>
2022-11-16 06:55:36 +0000

    new

---
+++ reportDEVtest.py
@@ -0,0 +1,14 @@
+import reportDEV
+
+a = reportDEV.getCPUreport()
+b = reportDEV.getRAMreport()
+c = reportDEV.getDISKreport()
+d = reportDEV.getIPreport()
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
