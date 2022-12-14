16da86c7823971e78b372edcc6ab6f1115cffeef
namyoung.cho <namyoung.cho@kewtea.com>
2022-12-14 04:14:19 +0000

    new: setting.py

--- send_api.py
+++ send_api.py
@@ -10,13 +10,8 @@ ip = reportDEV.getIPreport()
 cpu = reportDEV.getCPUreport()
 ram = reportDEV.getRAMreport()
 disk = reportDEV.getDISKreport()
-error = reportERROR.getApache2Error()
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome"}
 
 @app.get("/reports")
 async def getDEV():
-    return{"ip": ip ,"cpu": cpu, "ram": ram, "disk": disk, "error": error}
-
+    return{"ip": ip ,"cpu": cpu, "ram": ram, "disk": disk}
+#d
--- setting.py
+++ setting.py
@@ -2,5 +2,4 @@ import os
 
 os.system("sudo apt-get install sysstat")
 os.system("sudo apt install python3-pip")
-os.system("pip install fastapi")
-os.system("pip install uvicorn")
+os.system("pip install fastapi uvicorn")
